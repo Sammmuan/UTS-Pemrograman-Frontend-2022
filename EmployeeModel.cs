d68965f6732492a714d6dfe251ad186b12f8473f
Sammmuan <107909259+Sammmuan@users.noreply.github.com>
2022-12-20 14:25:56 +0700

    Add files via upload

--- EmployeeModel.cs
+++ EmployeeModel.cs
@@ -1,4 +1,3 @@
-
 // Employee class
 public class Employee
 {
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorAppWasm;
+using MudBlazor.Services;
+using System.Globalization;
+using Microsoft.JSInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
+builder.Services.AddLocalization();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
